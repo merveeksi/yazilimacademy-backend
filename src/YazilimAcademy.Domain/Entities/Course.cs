ee193e98f5978560216bedcf4b8dde40f0a8d6ae
ALPER TUNGA <alper@altu.dev>
2024-12-17 02:01:43 +0300

    [Feature]: New properties added to the Course entity.

--- src/YazilimAcademy.Domain/Entities/Course.cs
+++ src/YazilimAcademy.Domain/Entities/Course.cs
@@ -10,5 +10,11 @@ public class Course : EntityBase
     public string? Description { get; private set; }
     public CourseLevel Level { get; private set; }
     public bool IsActive { get; private set; }
+    public string? ThumbnailUrl { get; private set; }
+    public TimeSpan Duration { get; private set; }
+    public decimal Price { get; private set; }
+    public int ReviewCount { get; private set; }
+    public int Rating { get; private set; }
+    public int StudentCount { get; private set; }
     public ICollection<CourseCategory> CourseCategories { get; private set; } = [];
 }
