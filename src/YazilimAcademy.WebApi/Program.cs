aaffc9c6f7cb13f49c5804cd71d8fd4166ac2bcc
Alper Tunga <alper@altu.dev>
2024-12-17 23:54:45 +0300

    ✨ Enhance Program.cs to integrate application, infrastructure, and web API services with dependency injection

--- src/YazilimAcademy.WebApi/Program.cs
+++ src/YazilimAcademy.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using YazilimAcademy.Application;
+using YazilimAcademy.Infrastructure;
+using YazilimAcademy.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddWebApi();
 
 var app = builder.Build();
 
